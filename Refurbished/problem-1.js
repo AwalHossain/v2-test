4f0a07aa42461d1e25ce46e4981e8b4307d53b5a
Awalhossain <waqin21@gmail.com>
2024-09-22 11:18:47 +0600

    Refactor firstRepeatingCharacter function to find the earliest repeating character in a string

---
+++ Refurbished/problem-1.js
@@ -0,0 +1,31 @@
+function firstRepeatingCharacter(s) {
+  // Create a Map to store characters we've seen and their indices
+  const seen = new Map();
+
+  // Variable to keep track of the earliest repeat
+  let earliestRepeatIndex = Infinity;
+  let firstRepeat = -1;
+
+  // Iterate through the string
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (seen.has(char)) {
+      // If we've seen this character before and it's earlier than our current earliest
+      if (seen.get(char) < earliestRepeatIndex) {
+        earliestRepeatIndex = seen.get(char);
+        firstRepeat = char;
+      }
+    } else {
+      // If we haven't seen it, add it to the map with its index
+      seen.set(char, i);
+    }
+  }
+
+  // Return the first repeating character or -1 if none found
+  return firstRepeat;
+}
+
+// Test cases
+console.log(firstRepeatingCharacter("abbacddac")); // Should output 'a'
+console.log(firstRepeatingCharacter("xyzzoy")); // Should output 'y'
+console.log(firstRepeatingCharacter("abcdef")); // Should output -1
